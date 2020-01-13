42866c3ee2d5e4e2cb3d7b3160481631d5928140
RitaMarques <anarita247@gmail.com>
2020-01-13 20:18:26 +0000

    new version data

--- code/Code.py
+++ code/Code.py
@@ -4,6 +4,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
+import plotly.offline as pyo
 import plotly.graph_objects as go
 
 # DATAFRAMES
@@ -11,31 +12,36 @@ df_athletes = pd.read_excel(r'.\data\athlete_events.xlsx', 'athlete_events')
 df_participants = pd.read_excel(r'.\data\athlete_events.xlsx', 'participants')
 
 # QUERIES
-medals_country = df_athletes.groupby("ISO3")["Medal"].count().copy()
+medals_country = pd.DataFrame(df_athletes.groupby("ISO3")["Medal"].count().copy().astype(str))
 
 # CHOROPLETH
+#fazer dataframe
+df_participants['text'] = 'Country: ' + df_participants['Country'] + '<br>' + \
+                                        'Host City ' + df_participants['City'] + '<br>' + \
+                                        'Edition ' + df_participants['Edition'] + '<br>' + \
+                                        'Hosting Year ' + df_participants['Year'].astype(str)#, + '<br>' + \
+                                        #'Number of Medals ' + medals_country
+
+
 map = go.Figure(data=go.Choropleth(locations=df_athletes['ISO3'],
                                    locationmode='ISO-3',
                                    z=medals_country,
-                                   zmin=0,
-                                   zmax=2650,
+                                   #zmin=0,
+                                   #zmax=2650,
                                    #nticks=,
                                    showscale=True,
-                                   text=df_participants['Country'] + '<br>' + \
-                                            'Host City: ' + df_participants['City'] + '<br>' + \
-                                            df_participants['Edition'] + '<br>' + \
-                                            df_participants['Year'] + '<br>' + \
-                                            medals_country,
+                                   hovertext= df_participants['text'],
                                    colorscale='BrBG', #RdGy
                                    colorbar={'title': 'Total Number<br>of Medals'}
                                    ),
-                layout=dict(geo=dict(showframe=False,
+                layout=dict(geo=dict(landcolor='rgb(255, 255, 255)',
+                                     showframe=False,
                                      projection={'type': 'equirectangular'})
                             #sliders=sliders)
                             )
                 )
 
-map.show()
+pyo.plot(map)
 
 # LINE CHART
 
@@ -87,7 +93,8 @@ fig.update_layout(
     showlegend=False,
     plot_bgcolor='white'
 )
-# LINE CHART 
+
+# LINE POINT CHART
 
 area = go.Figure(data=go.Scatter(x=df_participants['Year'],
                                  y=df_participants['Countries'],
@@ -98,7 +105,8 @@ area = go.Figure(data=go.Scatter(x=df_participants['Year'],
                                            dash='dot'), # "solid", "dot", "dash", "longdash", "dashdot", or "longdashdot"
                                  # shape = "linear" | "spline" | "hv" | "vh" | "hvh" | "vhv"
                                  marker=dict(size=[40, 60, 80, 100],
-                                             color=[0, 1, 2, 3]),
+                                             color=[0, 1, 2, 3],
+                                             symbol='hexagon2-open'),
                                  showlegend=False
                                  ),
                  layout=dict(#title='',
@@ -106,3 +114,5 @@ area = go.Figure(data=go.Scatter(x=df_participants['Year'],
                              yaxis=dict(title='Number of Countries')
                              )
                  )
+
+# AREA CHART
