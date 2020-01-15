81d7403dde784711787978eaaea686e35c131ec2
larabtgo <lara_oliveira98@hotmail.com>
2020-01-15 15:55:51 +0000

    novo layout

--- code/FinalProject.py
+++ code/FinalProject.py
@@ -56,10 +56,10 @@ app.layout = html.Div([
             html.Div([
                 # Div 1.1.1.1. Title
                 html.Div([
-                    html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), style={'height':'20%', 'width':'10%'}),
+                    html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), id='logo'),
                     #html.Img(src=app.get_asset_url( '/images/Olympic-logo.png')),
                     html.P('Summer Olympics Games'),
-                ], id='title', className='title', style={'display': 'inline-block'},
+                ], id='title', className='title',
                 ),  # end div 1.1.1.1.
 
                 # Div 1.1.1.2. Nr of Editions
@@ -203,6 +203,241 @@ app.layout = html.Div([
 
 ], id='outer_div'
 )
+
+
+#--------------------------------------- NOVO LAYOUT---------------------------------------------#
+#------------------------------------------------------------------------------------------------
+#---------------------------------------------------------------------------------------------
+
+app.layout = html.Div([
+
+    # Div 1. - Title, Top Winners, Top countries, Filter, Search
+    html.Div([
+
+        # Div 1.1. - Title
+        html.Div([
+            html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), id='logo'),
+            #html.Img(src=app.get_asset_url( '/images/Olympic-logo.png')),
+            html.P('Summer Olympics Games')
+        ], id='title', className='title leftboxes'
+        ),  # end div 1.1.
+
+        # Div 1.2. - Top Winners
+        html.Div([
+            html.P('Top Winners'),
+            # dcc.Graph(
+            #         id='top_contries_fig'
+            # )
+        ], id='top_winners', className='leftboxes'
+        ),  # end div 1.2.
+
+        # Div 1.3. - Top Countries
+        html.Div([
+
+            html.P('Top Countries')
+
+        ], id='top_countries', className='leftboxes'
+        ),  # end div 1.3.
+
+        # Div 1.4. - Search, Filter
+        html.Div([
+            # Div 1.4.1. - Filter
+            hhtml.Div([
+                html.P('What Type of Sports do you want to see?'),
+                dcc.RadioItems(
+                    id='sport_type',
+                    options=[
+                        {'label': 'Colective', 'value': 'colective'},
+                        {'label': 'Individual', 'value': 'individual'},
+                        {'label': 'Both', 'value': 'both'},
+
+                    ],
+                    value='both',
+                    labelStyle={'display': 'inline-block'}
+                )
+            ], id='filter'
+            ),  # end div 1.4.1
+
+        ], id='filters', className='leftboxes'
+        ),  # end div 1.4.
+
+
+    ], id='div_1', className='column_1'
+    ),  # end div 1.
+
+
+
+    # Div 2. - Nr of Editions, HeatMap, Slider, Linecharts (Countries, Events, Athletes)
+    html.Div([
+
+    ], id='div_2', className='column_2'
+    ),  # end div 2.
+
+], id='outer_div'
+),
+
+app.layout = html.Div([
+
+    # Div 1. - Title, Top Winers, Top countries, Filter, Search
+    html.Div([
+
+        # Div 1.1. Title, Heatmap
+        html.Div([
+
+            # Div 1.1.1. Title, Nr of Editions, Cities, Countr
+            html.Div([
+                # Div 1.1.1.1. Title
+                html.Div([
+                    html.Img(src='data:image/png;base64,{}'.format(encoded_image.decode()), id='logo'),
+                    #html.Img(src=app.get_asset_url( '/images/Olympic-logo.png')),
+                    html.P('Summer Olympics Games'),
+                ], id='title', className='title',
+                ),  # end div 1.1.1.1.
+
+                # Div 1.1.1.2. Nr of Editions
+                html.Div([
+                    html.P('Number of Editions:'),
+                    html.P('XXXI')
+                ], id='nr_editions', className='minibox'
+                ),  # end div 1.1.1.2.
+
+                # Div 1.1.1.3. Cities Countries
+                html.Div([
+                    html.P('Number of Countries: '),
+                    html.Div([str(nr_countries)], className='nr'),
+                    html.P('Number of Host Cities: '),
+                    html.Div([str(nr_host_cities)], className='nr'),
+                    html.P('Number of Events: '),
+                    html.Div([str(nr_events)], className='nr')
+                ], id='details', className='minibox'
+                ),  # end div 1.1.1.3.
+
+            ], id='header', className='row_1_1'
+            ),  # end div 1.1.1.
+
+
+            # Div 1.1.2. HeatMap
+            html.Div([
+                html.P('HEATMAP'),
+                # dcc.Graph(id='heatmap', figure=map)
+            ], id='HeatMap', className='row_1_2'
+            ),  # end div 1.1.2.
+
+        ], id='inner_div_1', className='column_1'
+        ),  # end div 1.1.
+
+
+        # Div 1.2. - Top Winners, Top Countries
+        html.Div([
+            # Div 1.2.1. - Top Winners
+            html.Div([
+                html.P('Top Winners'),
+                # dcc.Graph(
+                #         id='top_contries_fig'
+                # )
+            ], id='top_winners', className='normalbox'
+            ),  # end div 1.2.1.
+
+            # Div 1.2.2. - Top Countries
+            html.Div([
+
+                html.P('Top Countries'),
+
+            ], id='top_countries', className='normalbox'
+            ),  # end div 1.2.2.
+
+        ], id='inner_div_2', className='column_2'
+        ),  # end div 1.2.
+
+    ], className='row_1'
+    ),  # end div 1.
+
+
+
+    # Div 2. - HeatMap, Slider, Linecharts (Countries, Events, Athletes)
+    html.Div([
+
+        # Div 2.1. - Slider, Linecharts (Countries, Events, Athletes)
+        html.Div([
+
+            # Div 2.1.1. - Slider
+            html.Div([
+                html.P('SLIDER'),
+                dcc.Slider(
+                    id='year-slider',
+                    min=1892,
+                    max=2016,
+                    #step=4,
+                    #marks=datedict,
+                    marks=years_select, #.insert(0, 'All')
+                    #tooltip=str(value),
+                    value=2016,
+                    included=False,
+                    persistence_type='session',
+                )
+
+
+            ], id='slider', className='row_2_1',
+            ),  # end div 2.1.1.
+
+
+            # Div 2.1.2. - Linechart/Barchart/Areachart - Countries, Sports, Athletes
+            html.Div([
+                # Div 2.1.2.1. - Linechart Countries
+                html.Div([
+                    html.P('Linechart Countries'),
+                 #   dcc.Graph(id='linechart', figure=line)
+                ], id='countries_linechart', className='boxes'
+                ),  # end div 2.1.2.1.
+
+                # Div 2.1.2.2. - Barchart Sports
+                html.Div([
+                    html.P('Barchart Sports'),
+                  #  dcc.Graph(id='linechart', figure=bar)
+                ], id='events_linechart', className='boxes'
+                ),  # end div 2.1.2.2.
+
+                # Div 2.1.2.3. - Areachart Men & Women
+                html.Div([
+                    html.P('Athletes Men & Women'),
+                  #  dcc.Graph(id='linechart', figure=area)
+                ], id='athletes_linechart', className='boxes'
+                ),  # end div 2.1.2.3.
+
+            ], id='linecharts', className='row_2_2'
+            ),  # end div 2.1.2.
+
+        ], id='inner_div_3', className='column_1'
+        ),  # end div 2.1
+
+
+        # Div 2.2. - Filter, Search
+        html.Div([
+            html.P('What Type of Sports do you want to see?'),
+            dcc.RadioItems(
+                id='sport_type',
+                options=[
+                    {'label': 'Colective', 'value': 'colective'},
+                    {'label': 'Individual', 'value': 'individual'},
+                    {'label': 'Both', 'value': 'both'},
+
+                ],
+                value='both',
+                labelStyle={'display': 'inline-block'}
+            ),
+        ], id='inner_div_4', className='column_2'
+        ),  # end div 2.2
+
+
+    ], className='row_2'
+    ),  # end div 2.
+
+
+], id='outer_div'
+)
+
+
+
 
 #----------------------------------------Callbacks---------------------------------------------------------------------#
 
